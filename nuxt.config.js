d03413be56ebe28c7dee641d7e654aa73951c1e3
shonubijerry <korede@verifi.ng>
2022-07-18 08:03:23 +0100

    fixes

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,41 +11,32 @@ const routerBase =
     : {}
 
 export default {
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
+  render: {
+    static: {
+      setHeaders(res) {
+        res.setHeader('Access-Control-Allow-Credentials', 'true')
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
+        res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
+        res.setHeader(
+          'Access-Control-Allow-Methods',
+          'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+        )
       },
-    ]
+    },
+    dist: {
+      setHeaders(res) {
+        res.setHeader('Access-Control-Allow-Credentials', 'true')
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
+        res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
+        res.setHeader(
+          'Access-Control-Allow-Methods',
+          'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+        )
+      },
+    },
   },
-  // render: {
-  //   static: {
-  //     // setHeaders(res) {
-  //     //   res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
-  //     //   res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
-  //     // },
-  //   },
-  //   dist: {
-  //     // setHeaders(res) {
-  //     //   res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
-  //     //   res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
-  //     // },
-  //   },
-  // },
   /*
    ** Concat router base setting
    */
